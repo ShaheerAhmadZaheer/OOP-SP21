61be1df5525643dac33afca817f6cce1d87cb4f6
ShaheerAhmadZaheer <80230799+ShaheerAhmadZaheer@users.noreply.github.com>
2021-03-15 11:22:23 +0500

    Create L5HA02_Runner.java

---
+++ Lab 5/Home Activity 02/L5HA02_Runner.java
@@ -0,0 +1,18 @@
+public class L5HA2_Runner {
+
+	public static void main(String[] args) {
+		Book b1 = new Book();
+		b1.setAuthor("S.A. Lewis");
+		String [] array1 = {"Chap1", "Chap2", "Chap3", "Chap4"};
+		b1.setChapterNames(array1);
+		
+		
+		String [] array2 = {"Chap2","Chap2","Chap3"};
+		Book b2 = new Book("S.A. Lewis", array2);
+		
+		System.out.println(b1.compareAuthors(b2));
+		b1.compareChapterNames(b2);
+
+	}
+
+}
