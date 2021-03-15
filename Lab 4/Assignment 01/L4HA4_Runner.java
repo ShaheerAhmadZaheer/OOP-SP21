976155e4ab2a7c63267254c8d502bdbf87053cd3
ShaheerAhmadZaheer <80230799+ShaheerAhmadZaheer@users.noreply.github.com>
2021-03-15 08:40:42 +0500

    Create L4HA4_Runner.java

---
+++ Lab 4/Assignment 01/L4HA4_Runner.java
@@ -0,0 +1,25 @@
+public class L4HA1_Runner {
+
+	public static void main(String[] args) {
+		HotDogStand s1 = new HotDogStand(1, 0);
+		s1.justSold();
+		s1.justSold();
+		s1.justSold();
+		
+		s1.display();
+		
+		HotDogStand s2 = new HotDogStand(2, 4);
+		s2.justSold();
+		s2.justSold();
+		
+		s2.display();
+		
+		HotDogStand s3 = new HotDogStand(3, 1);
+		s3.justSold();
+		s3.justSold();
+		s3.justSold();
+		s3.justSold();
+		
+		s3.display();
+
+	}
