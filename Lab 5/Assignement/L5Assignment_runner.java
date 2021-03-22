cec5ec2b2b18c6c041d9a2c44d755b78e9b955fb
ShaheerAhmadZaheer <80230799+ShaheerAhmadZaheer@users.noreply.github.com>
2021-03-22 09:49:47 +0500

    Create L5Assignment_runner.java

---
+++ Lab 5/Assignement/L5Assignment_runner.java
@@ -0,0 +1,12 @@
+public class L5Assignment_Runner {
+
+	public static void main(String[] args) {
+		Fraction f1 = new Fraction();
+		System.out.println("ratio of Fration is equalls to:" +f1.getRatio());
+		
+		Fraction f2 = new Fraction(48,24);
+		System.out.println("ratio Fration 1 is equals to ratio of Fraction 2? "+f2.equals(f1));
+
+	}
+
+}
