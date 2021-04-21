72e244ae722ff6617b7667fa8862925e7d2db10e
ShaheerAhmadZaheer <80230799+ShaheerAhmadZaheer@users.noreply.github.com>
2021-04-21 12:03:34 +0500

    Create Person.java

---
+++ Lab 08/Home Activities/Person.java
@@ -0,0 +1,17 @@
+public class Person {
+	 protected String name;
+	 protected String address;
+	 protected int phonenumber;
+	 protected String emailaddress;
+	    
+	    
+	    
+	 public Person( String name, String address, int phonenumber, String emailaddress) {
+	      this.name = name;
+	      this.address = address;
+	      this.phonenumber = phonenumber;
+	      this.emailaddress = emailaddress;
+	        
+	 }
+
+}
